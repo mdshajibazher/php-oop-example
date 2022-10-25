e7fa62c098ea06c7df364c47b51a41fe4e11e6ef
mdshajibazher <mdshajibazher@gmail.com>
2022-10-25 10:06:09 +0600

    object cloning another problem

--- object_clonning.php
+++ object_clonning.php
@@ -1,16 +1,38 @@
 <?php
 
+Class Color{
+    private $color;
+    function __construct($color){
+        $this->color = $color;
+    }
+
+    function setColor($color){
+        $this->color = $color;
+    }
+}
 
 class favColor{
     private $data;
+    private $color;
 
     function __construct($data_argument)
     {
         $this->data = $data_argument;
+        $this->color = new Color("Red");
+    }
+
+    function updateColor($color){
+        $this->color->setColor($color);
     }
 
     function setData($data){
         $this->data = $data;
+    }
+
+    //To Solve clone issue
+
+    function __clone(){
+        $this->color = clone $this->color;
     }
 }
 
@@ -27,4 +49,9 @@ $fc3->setData("Clonned data");
 print_r($fc2);
 print_r($fc3);
 
+// A problem arise in clone
 
+$fc4 = clone($fc1);
+$fc4->updateColor("Yellow");
+print_r($fc1);
+print_r($fc4);
