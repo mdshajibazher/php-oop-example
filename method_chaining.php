b25704619651b8f402503462c3415aa95b47d64a
mdshajibazher <mdshajibazher@gmail.com>
2022-10-28 08:36:41 +0600

    method chaining example

---
+++ method_chaining.php
@@ -0,0 +1,46 @@
+<?php
+
+class StringUtility{
+    private $string;
+    private $search;
+    function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    function search($text){
+        $this->search = $text;
+        return $this;
+    }
+
+    function replace($text){
+        if(!isset($this->search)){
+            throw new Exception("Nothing to replace");
+        }
+
+        $this->string = str_replace($this->search,$text,$this->string);
+        return $this;
+    }
+
+    function uppercase(){
+        $this->string = strtoupper($this->string);
+        return $this;
+    }
+
+    function lowercase(){
+        $this->string = strtolower($this->string);
+        return $this;
+    }
+
+    function print(){
+        echo $this->string;
+    }
+
+}
+
+
+
+
+
+$s = new StringUtility("Hello World");
+$s->search("World")->replace("Peter")->uppercase()->print();
